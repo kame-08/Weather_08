82ad88ba4f92fd924c2a4bb564316c16e97805e8
Kame15-2021 <21cm0108@jec.ac.jp>
2022-07-19 09:49:17 +0900

    Refactor:メンテナンス

--- Weather_08/ContentView.swift
+++ Weather_08/ContentView.swift
@@ -10,130 +10,130 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var weatherDateList = WeatherDate()
     
-    @State var aiu = "aaaaa"
-    @State var kakiku = "kkk"
+    //未実装パーツ
+    @State var hoge = "hoge"
+    //未実装パーツ
+    @State var fuga = "fuga"
     
     var body: some View {
-//        ZStack{
-//            ProgressView("通信中...")
-//                .onAppear {
-//                    weatherDateList.searchWeather(keyword: aiu)
-//                }
-            
+        //        ZStack{
+        //            ProgressView("通信中...")
+        //                .onAppear {
+        //                    weatherDateList.searchWeather(keyword: aiu)
+        //                }
         
-            VStack {
-                if let weather = weatherDateList.weatherList.first{
-                    if weather.main == "Thunderstorm"{
-                        Image(systemName: "cloud.bolt.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        
-                        
-                    }else if weather.main == "Drizzle"{
-                        Image(systemName: "cloud.rain.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        // .symbolRenderingMode(.multicolor)
-                    }else if weather.main == "Rain"{
-                        Image(systemName: "cloud.rain.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        //  .symbolRenderingMode(.multicolor)
-                    }else if weather.main == "Snow"{
-                        Image(systemName: "snowflake")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        //  .symbolRenderingMode(.multicolor)
-                    } else if weather.main == "Clear"{
-                        Image(systemName: "sun.max.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        //  .symbolRenderingMode(.multicolor)
-                    }else if weather.main == "Clouds"{
-                        Image(systemName: "cloud.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        //  .symbolRenderingMode(.multicolor)
-                    }else {
-                        Image(systemName: "moon.fill")
-                            .resizable()
-                            .scaledToFit()
-                        
-                            .frame(width: 150, height: 150)
-                        
-                        //  .symbolRenderingMode(.multicolor)
-                    }
+        VStack {
+            if let weather = weatherDateList.weatherList.first{
+                if weather.main == "Thunderstorm"{
+                    Image(systemName: "cloud.bolt.fill")
+                        .resizable()
+                        .scaledToFit()
                     
+                        .frame(width: 150, height: 150)
+                    
+                }else if weather.main == "Drizzle"{
+                    Image(systemName: "cloud.rain.fill")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    // .symbolRenderingMode(.multicolor)
+                }else if weather.main == "Rain"{
+                    Image(systemName: "cloud.rain.fill")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    //  .symbolRenderingMode(.multicolor)
+                }else if weather.main == "Snow"{
+                    Image(systemName: "snowflake")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    //  .symbolRenderingMode(.multicolor)
+                } else if weather.main == "Clear"{
+                    Image(systemName: "sun.max.fill")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    //  .symbolRenderingMode(.multicolor)
+                }else if weather.main == "Clouds"{
+                    Image(systemName: "cloud.fill")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    //  .symbolRenderingMode(.multicolor)
+                }else {
+                    Image(systemName: "moon.fill")
+                        .resizable()
+                        .scaledToFit()
+                    
+                        .frame(width: 150, height: 150)
+                    
+                    //  .symbolRenderingMode(.multicolor)
                 }
                 
-
-                Text(weatherDateList.place)
-                    .font(.title)
-                    .onAppear(){
-                        weatherDateList.searchWeather(keyword: aiu)
-                    }
-                List(weatherDateList.weatherList) {weather in
-                     
-                     HStack{
-                         
-                         Text(weather.dt)
-                         Spacer()
-                         // Text(weather.main)
-                         if weather.main == "Thunderstorm"{
-                             Image(systemName: "cloud.bolt.fill")
-                             
-                                 .symbolRenderingMode(.multicolor)
-                         }else if weather.main == "Drizzle"{
-                             Image(systemName: "cloud.rain.fill")
-                             
-                             // .symbolRenderingMode(.multicolor)
-                         }else if weather.main == "Rain"{
-                             Image(systemName: "cloud.rain.fill")
-                             
-                             //  .symbolRenderingMode(.multicolor)
-                         }else if weather.main == "Snow"{
-                             Image(systemName: "snowflake")
-                             //  .symbolRenderingMode(.multicolor)
-                         } else if weather.main == "Clear"{
-                             Image(systemName: "sun.max.fill")
-                             //  .symbolRenderingMode(.multicolor)
-                         }else if weather.main == "Clouds"{
-                             Image(systemName: "cloud.fill")
-                             //  .symbolRenderingMode(.multicolor)
-                         }else {
-                             Image(systemName: "moon.fill")
-                             //  .symbolRenderingMode(.multicolor)
-                         }
-                         Spacer()
-                         Text("\(Int(weather.tempMin))°")
-                         Spacer()
-                         Text("\(Int(weather.tempMax))°")
-                         
-                     }
-                 }
-                 .refreshable{
-                     weatherDateList.searchWeather(keyword: aiu)
-                 }
-                
-            }.onAppear {
-                weatherDateList.searchWeather(keyword: aiu)
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    weatherDateList.searchWeather(keyword: aiu)
-                }
             }
-//        }
+            
+            
+            Text(weatherDateList.place)
+                .font(.title)
+                .onAppear(){
+                    weatherDateList.searchWeather(keyword: hoge)
+                }
+            List(weatherDateList.weatherList) {weather in
+                 
+                 HStack{
+                     
+                     Text(weather.dt)
+                     Spacer()
+                     // Text(weather.main)
+                     if weather.main == "Thunderstorm"{
+                         Image(systemName: "cloud.bolt.fill")
+                         
+                             .symbolRenderingMode(.multicolor)
+                     }else if weather.main == "Drizzle"{
+                         Image(systemName: "cloud.rain.fill")
+                         
+                         // .symbolRenderingMode(.multicolor)
+                     }else if weather.main == "Rain"{
+                         Image(systemName: "cloud.rain.fill")
+                         
+                         //  .symbolRenderingMode(.multicolor)
+                     }else if weather.main == "Snow"{
+                         Image(systemName: "snowflake")
+                         //  .symbolRenderingMode(.multicolor)
+                     } else if weather.main == "Clear"{
+                         Image(systemName: "sun.max.fill")
+                         //  .symbolRenderingMode(.multicolor)
+                     }else if weather.main == "Clouds"{
+                         Image(systemName: "cloud.fill")
+                         //  .symbolRenderingMode(.multicolor)
+                     }else {
+                         Image(systemName: "moon.fill")
+                         //  .symbolRenderingMode(.multicolor)
+                     }
+                     Spacer()
+                     Text("\(Int(weather.tempMin))°")
+                     Spacer()
+                     Text("\(Int(weather.tempMax))°")
+                     
+                 }
+             }
+             .refreshable{
+                 weatherDateList.searchWeather(keyword: hoge)
+             }
+            
+        }.onAppear {
+            weatherDateList.searchWeather(keyword: hoge)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                weatherDateList.searchWeather(keyword: hoge)
+            }
+        }
+        //        }
     }
 }
 
--- Weather_08/WeatherDate.swift
+++ Weather_08/WeatherDate.swift
@@ -10,6 +10,109 @@ import UIKit
 import SwiftUI
 import CoreLocation
 
+//検索用クラス
+class WeatherDate: ObservableObject{
+    
+    //let appid:String = <#ここにAPIKey#>
+    
+    var place = ""
+    //Weatherのリスト
+    @Published var weatherList: [WeatherItem] = []
+    
+    func searchWeather(keyword: String) {
+        
+        print("------")
+        print(keyword)
+        
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            print("return1")
+            return
+        }
+        
+        //リクエストURLの組み立て
+        guard let req_url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=35.698785&lon=139.696548&exclude=current,minutely,hourly,alerts&units=metric&"+"appid="+appid+"&lang=ja") else {
+            print("return2")
+            return
+        }
+        
+        print("リクエストURL")
+        print(req_url)
+        
+        // リクエストに必要な情報を生成
+        let req = URLRequest(url: req_url)
+        // データ転送を管理するためのセッションを生成
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // リクエストをタスクとして登録
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, response ,error) in
+            // セッションを終了
+            session.finishTasksAndInvalidate()
+            // do try catch エラーハンドリング
+            do {
+                // JSONDecoderのインスタンス取得
+                let decoder = JSONDecoder()
+                // 受け取ったJSONデータをパース（解析）して格納
+                let json = try decoder.decode(WeatherJson.self, from: data!)
+                
+                //print(json)
+                
+                //リストの初期化
+                self.weatherList.removeAll()
+                var count = 0
+                
+                for item in json.daily {
+                    
+                    //取得してる数だけ処理
+                    //for daily in dailys {
+                    count += 1
+                    
+                    // UNIX時間 "dateUnix" をNSDate型 "date" に変換
+                    let dateUnix: TimeInterval = TimeInterval(item.dt)
+                    let date = NSDate(timeIntervalSince1970: dateUnix)
+                    
+                    // NSDate型を日時文字列に変換するためのNSDateFormatterを生成
+                    let formatter = DateFormatter()
+                    
+                    //yyyy年MM月dd日
+                    formatter.dateFormat = "d日"
+                    
+                    // NSDateFormatterを使ってNSDate型 "date" を日時文字列 "dateStr" に変換
+                    let dateStr: String = formatter.string(from: date as Date)
+                    // print(dateStr)
+                    
+                    //日付、天気をまとめて管理
+                    //daily.dt⇨dateStr
+                    let weather = WeatherItem(num: count, dt:dateStr , main: item.weather.first!.main, tempDay: item.temp.day, tempMin:item.temp.min ,tempMax: item.temp.max)
+                    //weather配列へ追加
+                    self.weatherList.append(weather)
+                    
+                    print("リスト")
+                    print(self.weatherList)
+                    
+                    let location = CLLocation(latitude: json.lat, longitude: json.lon)
+                    getLocation(from: location) { placemark in
+                        self.place = placemark.locality!
+                    }
+                    
+                }
+                
+                //let location = CLLocation(latitude: json.lat, longitude: json.lon)
+                //getLocation(from: location) { placemark in
+                //elf.place = placemark.locality!
+                //}
+                
+            } catch {
+                // エラー処理
+                print("エラーが出ました")
+            }
+        })
+        // ダウンロード開始
+        task.resume()
+    }
+    
+}
+
+
 struct WeatherItem: Identifiable {
     let id = UUID()
     let num :Int
@@ -25,15 +128,13 @@ struct WeatherItem: Identifiable {
 //
 //   let weatherData = try? newJSONDecoder().decode(WeatherData.self, from: jsonData)
 
-
-
 // MARK: - WeatherData
 struct WeatherJson: Codable {
     let lat, lon: Double
     let timezone: String
     let timezoneOffset: Int
     let daily: [Daily]
-
+    
     enum CodingKeys: String, CodingKey {
         case lat, lon, timezone
         case timezoneOffset = "timezone_offset"
@@ -56,7 +157,7 @@ struct Daily: Codable {
     let clouds: Int
     let pop, uvi: Double
     let rain: Double?
-
+    
     enum CodingKeys: String, CodingKey {
         case dt, sunrise, sunset, moonrise, moonset
         case moonPhase = "moon_phase"
@@ -86,7 +187,7 @@ struct Temp: Codable {
 struct Weather: Codable {
     let id: Int
     let main, weatherDescription, icon: String
-
+    
     enum CodingKeys: String, CodingKey {
         case id, main
         case weatherDescription = "description"
@@ -94,115 +195,10 @@ struct Weather: Codable {
     }
 }
 
-
-
 private func getLocation(from location: CLLocation, completion: @escaping (_ placemark: CLPlacemark) -> Void) {
     let geocoder = CLGeocoder()
     geocoder.reverseGeocodeLocation(location) { placemarks, error in
         guard let placemark = placemarks?.first, error == nil else { return }
         completion(placemark)
     }
-}
-
-
-
-//検索用クラス
-class WeatherDate: ObservableObject{
-
-    var place = ""
-    //Weatherのリスト
-    @Published var weatherList: [WeatherItem] = []
-    
-    
-    func searchWeather(keyword: String) {
-        
-        print("------")
-        print(keyword)
-        
-        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            print("return1")
-            return
-        }
-        
-        //リクエストURLの組み立て
-        guard let req_url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=35.698785&lon=139.696548&exclude=current,minutely,hourly,alerts&units=metric&appid=8defd96e2f076346d90939073960c0d0&lang=ja") else {
-            print("return2")
-            return
-        }
-        
-//        print("リクエストURL")
-//        print(req_url)
-        
-        // リクエストに必要な情報を生成
-        let req = URLRequest(url: req_url)
-        // データ転送を管理するためのセッションを生成
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        // リクエストをタスクとして登録
-        let task = session.dataTask(with: req, completionHandler: {
-            (data, response ,error) in
-            // セッションを終了
-            session.finishTasksAndInvalidate()
-            // do try catch エラーハンドリング
-            do {
-                // JSONDecoderのインスタンス取得
-                let decoder = JSONDecoder()
-                // 受け取ったJSONデータをパース（解析）して格納
-                let json = try decoder.decode(WeatherJson.self, from: data!)
-                
-//                                print(json)
-                
-                //リストの初期化
-                self.weatherList.removeAll()
-                var count = 0
-                
-                for item in json.daily {
-                    
-                    //取得してる数だけ処理
-//                    for daily in dailys {
-                        count += 1
-                        
-                        // UNIX時間 "dateUnix" をNSDate型 "date" に変換
-                        let dateUnix: TimeInterval = TimeInterval(item.dt)
-                        let date = NSDate(timeIntervalSince1970: dateUnix)
-
-                        // NSDate型を日時文字列に変換するためのNSDateFormatterを生成
-                        let formatter = DateFormatter()
-                        
-                        //yyyy年MM月dd日
-                        formatter.dateFormat = "d日"
-
-                        // NSDateFormatterを使ってNSDate型 "date" を日時文字列 "dateStr" に変換
-                        let dateStr: String = formatter.string(from: date as Date)
-                       // print(dateStr)
-
-                            //日付、天気をまとめて管理
-                        //daily.dt⇨dateStr
-                        let weather = WeatherItem(num: count, dt:dateStr , main: item.weather.first!.main, tempDay: item.temp.day, tempMin:item.temp.min ,tempMax: item.temp.max)
-                            //weather配列へ追加
-                    self.weatherList.append(weather)
-
-                    print("リスト")
-                    print(self.weatherList)
-                    
-                    let location = CLLocation(latitude: json.lat, longitude: json.lon)
-                    getLocation(from: location) { placemark in
-                        self.place = placemark.locality!
-                    }
-
-                }
-            
-//                let location = CLLocation(latitude: json.lat, longitude: json.lon)
-//                getLocation(from: location) { placemark in
-//                    self.place = placemark.locality!
-//                }
-                
-            } catch {
-                // エラー処理
-                print("エラーが出ました")
-            }
-        })
-        // ダウンロード開始
-        task.resume()
-    }
-    
 }
